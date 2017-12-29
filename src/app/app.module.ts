4c6b1a1f4d9bf2af5a6f304293e0c136d17b1c6b
amehlen <mehlen.allan@gmail.com>
2017-12-29 15:14:57 +0100

    import jquery and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { IcehockeyComponent } from './icehockey/icehockey.component';
 import { FootballComponent } from './football/football.component';
