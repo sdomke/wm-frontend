8e3d5ba16887588cc0aeb5a425a49ee350e68c0a
sdomke <s.o.domke@web.de>
2017-12-29 17:48:07 +0100

    Bugfix icehockey component

--- src/app/icehockey/icehockey.component.ts
+++ src/app/icehockey/icehockey.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from '../data-service.service';
 
 @Component({
   selector: 'app-icehockey',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IcehockeyComponent implements OnInit {
 
-  constructor() { }
+  public message: string;
+  public values: any[];
+
+  constructor(private _dataService: DataServiceService) {
+    this.message = 'Hello from HomeComponent constructor';
+  }
 
   ngOnInit() {
+    this._dataService
+      .getAll<any[]>()
+      .subscribe((data: any[]) => this.values = data,
+        error => () => {
+          console.log('error', 'Damn', 'Something went wrong...');
+        },
+        () => {
+          console.log('success', 'Complete', 'Getting all values complete');
+        });
   }
 
 }
