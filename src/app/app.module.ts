fa1852cbc17c928d7990e9af84aaebd21775e69c
amehlen <mehlen.allan@gmail.com>
2017-12-29 14:33:31 +0100

    add football component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FootballComponent } from './football/football.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FootballComponent
   ],
   imports: [
     BrowserModule
